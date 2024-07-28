8a3e94dce10f5cda55cc7127ef9d63141710418c
nishant <nishantchaurasiya1108@gmail.com>
2024-07-28 17:49:33 +0530

    add animation on sun image and write some js code

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-const checkbox=document.querySelectorAll(".ellipse");
+const checkboxes=document.querySelectorAll(".ellipse");
 const progressValue=document.querySelector(".progress-value");
-const  input=document.querySelectorAll(".task")
+const  inputs=document.querySelectorAll(".task");
+const error=document.querySelector(".error");
+const images=document.querySelectorAll(".checkbox");
+
+checkboxes.forEach((checkbox)=>{
+ checkbox.addEventListener("click",()=>{
+   const allInputFilled=[...inputs].every((input)=>{
+    console.log(input.value);
+    return input.value
+   });
+   if(allInputFilled){
+    checkbox.parentElement.classList.add("completed")
+   }else{
+    error.classList.remove("hide");
+   }
+   inputs.forEach((input)=>{
+    input.addEventListener("focus",()=>{
+      error.classList.add("hide")
+    })
+   })
+ });
+});
